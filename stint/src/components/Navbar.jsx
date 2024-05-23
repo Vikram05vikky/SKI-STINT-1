fa44fbbe433c3c31f857e67bf96c2e95690da5f0
Vikram05vikky <vikram05sar@gmail.com>
2024-05-23 19:06:46 +0530

    sidebar styles

--- stint/src/components/Navbar.jsx
+++ stint/src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./css/Navbar.css"
 import DarkMode from './DarkMode'
+import img from './images/namelogo.png'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 export default function Navbar() {
   return (
     <div className='nav-container'>
+      <div className='image-src'>
+    <img src={img} alt="" />
+    </div>
     <h2>SKI-STINT</h2>
     <div className='nav-icon-container' >
+    
     <DarkMode/>
     <AccountCircleIcon fontSize='large'  style={{marginTop:"2px",color:"var(--nav-hover)"}}/>
     </div>
