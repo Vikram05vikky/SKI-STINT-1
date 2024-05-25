1ce806910bd52b0c4b1892a10787866bc5c834be
Vikram05vikky <vikram05sar@gmail.com>
2024-05-25 16:03:57 +0530

    change

--- stint/src/components/Status.jsx
+++ stint/src/components/Status.jsx
@@ -1,8 +1,4 @@
 import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import {useNavigate } from "react-router-dom";
-
-import viteLogo from "/vite.svg";
 import "./css/Status.css";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
--- stint/src/components/YetToStart.jsx
+++ stint/src/components/YetToStart.jsx
@@ -1,7 +1,4 @@
 
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./css/LandingPage.css";
 import Navbar from "./Navbar";
 
